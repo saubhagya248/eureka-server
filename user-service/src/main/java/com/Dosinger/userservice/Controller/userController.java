4cc98a2973226faa00ee478f745c754c76a725d4
saubhagya248 <58850321+saubhagya248@users.noreply.github.com>
2023-06-19 23:43:21 +0000

    added user service
    ;
    :

---
+++ user-service/src/main/java/com/Dosinger/userservice/Controller/userController.java
@@ -0,0 +1,16 @@
+package com.Dosinger.userservice.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+// @RestController
+// @RequestMapping("/user")
+// public class userController{
+
+//     @PostMapping("/add")
+//     public ResponseEntity addUser(@RequestBody User user){
+//         try{
+
+//         }
+//     }
+//}
