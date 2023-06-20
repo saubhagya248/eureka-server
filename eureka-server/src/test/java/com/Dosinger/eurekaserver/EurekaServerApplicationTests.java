df2c8434ccde5052b3ccee95fdfd31c7996703ba
saubhagya248 <58850321+saubhagya248@users.noreply.github.com>
2023-06-20 00:51:44 +0000

    added eureka server

--- eureka-server/src/test/java/com/Dosinger/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/Dosinger/eurekaserver/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.Dosinger.userservice;
+package com.Dosinger.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class EurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
